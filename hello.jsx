5e2476ff98884e3337ddb8e954f3c9b7a5693e5a
daigo3 <stain.witness@gmail.com>
2016-04-23 23:34:40 +0900

    [add] react basics

--- hello.jsx
+++ hello.jsx
@@ -1,5 +1,47 @@
+const ClickCounterButton = React.createClass({
+  render: function() {
+    return (
+      <button onClick={this.props.clickHandler}>Don't click {this.props.times} times!</button>
+    );
+  }
+});
+
 const HelloWorld = React.createClass({
-  render: () => <h1>Hello, component!</h1>
+
+  getInitialState: function() {
+
+    var _this = this;
+    setInterval(() => {
+      _this.setState({ a: Math.random() });
+    }, 300);
+
+    return { a: 0, count: 0 };
+  },
+
+  getName: function() {
+    return 'aoi';
+  },
+
+  click: function() {
+    this.setState({ count: this.state.count + 1 });
+    if(this.state.count >= 2) {
+      alert('bomb');
+      this.setState({count: 0});
+    }
+  },
+
+  render: function() {
+
+    return (
+      <div>
+        <h1>hello, {this.getName()}.</h1>
+        <p>random number: {this.state.a}</p>
+        <br/>
+        <p>count: {this.state.count}</p>
+        <ClickCounterButton times={3} clickHandler={this.click}/>
+      </div>
+    );
+  }
 });
 
 ReactDOM.render(<HelloWorld/>, document.getElementById('example'));
